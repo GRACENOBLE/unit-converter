82568e6a77669f324b4ab90ae8ba855109dd0ef9
GRACENOBLE <gracenoble72@gmail.com>
2024-11-19 21:33:44 +0300

    feat(api): connected api routes to backend

---
+++ go-backend/api/GetMeasurements.go
@@ -0,0 +1,28 @@
+package api
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
+
+func GetMeasurements() {
+	url := "https://measurement-unit-converter.p.rapidapi.com/measurements"
+
+	req, _ := http.NewRequest("GET", url, nil)
+
+	req.Header.Add("x-rapidapi-key", "83bf703b51msh7ae20d0f1148c8fp157141jsnae9300ae1e01")
+	req.Header.Add("x-rapidapi-host", "measurement-unit-converter.p.rapidapi.com")
+
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer res.Body.Close()
+	body, _ := io.ReadAll(res.Body)
+
+	//fmt.Println(res)
+	fmt.Println(string(body))
+}
--- go-backend/main.go
+++ go-backend/main.go
@@ -1,5 +1,16 @@
 package main
 
-func main(){
-	
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request){
+		w.Write([]byte("Hello from a Go program"))
+	} )
+	err := http.ListenAndServe(":3333", nil)
+	if err != nil {
+		log.Fatal("Error opening server")
+	}
 }
